6d0c252281b71d0a38f1116fcd1de68989997196
Dalton <Dalton@Home.computer>
2021-05-23 01:21:58 -0600

    Fixed broken test, added matblazor, and modified the main page.

--- Daltonsbaker.Com/Program.cs
+++ Daltonsbaker.Com/Program.cs
@@ -1,11 +1,8 @@
+using MatBlazor;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
 using System;
-using System.Collections.Generic;
 using System.Net.Http;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace Daltonsbaker.Com
@@ -18,6 +15,7 @@ namespace Daltonsbaker.Com
             builder.RootComponents.Add<App>("#app");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddMatBlazor();
 
             await builder.Build().RunAsync();
         }

737ce7adbff789b1ce010dc993b95a84ceb6f931
Dalton <Dalton@Home.computer>
2021-05-23 00:01:02 -0600

    Added a better project settup with better naming

---
+++ Daltonsbaker.Com/Pages/FetchData.razor.cs
@@ -0,0 +1,21 @@
+﻿using Daltonsbaker.Com.Common;
+using Microsoft.AspNetCore.Components;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+
+namespace Daltonsbaker.Com.Pages
+{
+    public class FetchDataBase : ComponentBase
+    {
+        [Inject]
+        public HttpClient Http { get; set; }
+
+        protected WeatherForecast[] forecasts;
+
+        protected override async Task OnInitializedAsync()
+        {
+            forecasts = await Http.GetFromJsonAsync<WeatherForecast[]>("sample-data/weather.json");
+        }
+    }
+}

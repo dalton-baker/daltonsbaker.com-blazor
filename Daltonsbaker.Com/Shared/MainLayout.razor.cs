8a3e637ef7ff104eb0d1d704bb03a42923e4713b
Dalton <Dalton@Home.computer>
2021-07-19 19:24:46 -0600

    Changed the layout to a virtical scroll style

--- Daltonsbaker.Com/Shared/MainLayout.razor.cs
+++ Daltonsbaker.Com/Shared/MainLayout.razor.cs
@@ -6,43 +6,5 @@ namespace Daltonsbaker.Com.Shared
 {
     public partial class MainLayout
     {
-        private string _arrowColor = "#594ae2";
-        private int _selectedIndex = -1;
-        private MudCarousel<object> _mudCarousel;
-
-        private int SelectedIndex {
-            get => _selectedIndex;
-            set {
-                Debug.WriteLine("IndexChanged " + value);
-                _selectedIndex = value;
-            }
-        }
-
-        private void OnSwipe(SwipeDirection direction)
-        {
-            switch(direction)
-            {
-                case SwipeDirection.LeftToRight:
-                    _mudCarousel.Next();
-                    break;
-                case SwipeDirection.RightToLeft:
-                    _mudCarousel.Previous();
-                    break;
-            }
-
-            StateHasChanged();
-        }
-
-        private void SelectedIndexChanged(int index)
-        {
-            Console.WriteLine("IndexChanged");
-
-            _arrowColor = index switch
-            {
-                3 => "#000000",
-                _ => "#FFFFFF",
-            };
-            StateHasChanged();
-        }
     }
 }
